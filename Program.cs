6e29ae6699661454b6ab9555f5a7a4c5771714c7
KamoEllen <kamoellenkganakga@gmail.com>
2024-11-11 18:26:38 +0200

    Deleted folder and added new files

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;  
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = "hi.txt";
+        File.WriteAllText(filePath, "Hello World");
+       
+        string fileContent = File.ReadAllText(filePath);
+        Console.WriteLine(fileContent);
+    }
+}
